1ca9106f56d07d0697d4b2bd1b200756ab04521c
Dylan Wilson <dylan@bytepen.com>
2024-01-21 15:46:49 -0500

    Trying to wrap file contents in strings

--- libvirt-vm/ssh.tf
+++ libvirt-vm/ssh.tf
@@ -1,19 +1,19 @@
 resource "ssh_resource" "example" {
-  host         = libvirt_domain.main[0].network_interface.0.addresses.0
-  count        = data.coder_workspace.me.start_count
-  user         = "user"
+  host        = libvirt_domain.main[0].network_interface.0.addresses.0
+  count       = data.coder_workspace.me.start_count
+  user        = "user"
   private_key = tls_private_key.ssh_key.private_key_openssh
-
-  when         = "create"
+  timeout     = "1m"
+  when        = "create"
 
   file {
-    content     = data.coder_workspace.me.access_url
+    content     = "${data.coder_workspace.me.access_url}"
     destination = "~/.config/coder/url"
     permissions = "0400"
   }
 
   file {
-    content     = coder_agent.main.token
+    content     = "${coder_agent.main.token}"
     destination = "~/.config/coder/token"
     permissions = "0400"
   }
