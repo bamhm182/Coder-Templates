4154d49ef9221cbdfe743e8db5acbac89214b341
Dylan Wilson <dylan@bytepen.com>
2024-11-14 19:33:25 -0500

    k3s: trying a bad path to see if error changes

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -12,8 +12,8 @@ resource "libvirt_network" "internal" {
 }
 
 module "node0" {
-  source = "./modules/node"
-  #source = "git::https://github.com/bamhm182/Coder-Templates.git//libvirt-k3s/modules/node?ref=wip-k3s"
+  #source = "./modules/node"
+  source = "git::https://github.com/bamhm182/Coder-Templates.git//libvirt-k3s/modules/node/trying-a-bad-path?ref=wip-k3s"
   count  = data.coder_workspace.me.start_count
 
   coder_url  = data.coder_workspace.me.access_url
