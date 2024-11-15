766c4c56f250837eef6ad1aaeca2a974097998aa
Dylan Wilson <dylan@bytepen.com>
2024-11-14 21:00:17 -0500

    k3s: trying to loop module creation

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -10,47 +10,18 @@ resource "libvirt_network" "internal" {
   addresses = [ "127.${random_integer.network[0].result}.${random_integer.network[1].result}.0/24" ]
 }
 
-module "node0" {
+module "node" {
   source   = "./modules/node"
+  count    = 3
 
   agent_id      = coder_agent.Kubernetes.id
-  agent_token   = coder_agent.Kubernetes.token
+  agent_token   = count.index == 0 ? coder_agent.Kubernetes.token : "xxxxxxxxxX"
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
   network_id    = libvirt_network.internal.id
   owner         = data.coder_workspace.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
-  type          = "server"
+  type          = count.index == 0 ? "server" : "agent"
   ws_name       = data.coder_workspace.me.name
-  ws_number     = 0
-}
-
-module "node1" {
-  source   = "./modules/node"
-
-  agent_id      = coder_agent.Kubernetes.id
-  agent_token   = "xxxxxxx"
-  coder_url     = data.coder_workspace.me.access_url
-  cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
-  owner         = data.coder_workspace.me.name
-  ram           = (data.coder_parameter.ram_amount.value * 1024)
-  type          = "agent"
-  ws_name       = data.coder_workspace.me.name
-  ws_number     = 1
-}
-
-module "node2" {
-  source   = "./modules/node"
-
-  agent_id      = coder_agent.Kubernetes.id
-  agent_token   = "xxxxxxx"
-  coder_url     = data.coder_workspace.me.access_url
-  cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
-  owner         = data.coder_workspace.me.name
-  ram           = (data.coder_parameter.ram_amount.value * 1024)
-  type          = "agent"
-  ws_name       = data.coder_workspace.me.name
-  ws_number     = 2
+  ws_number     = count.index
 }
