09c7d695ab8ebd49d5988db146dd08219c665175
Dylan Wilson <dylan@bytepen.com>
2024-11-14 19:07:31 -0500

    k3s: fixed up providers

---
+++ libvirt-k3s/modules/node/providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "1.0.4"
+    }
+    guacamole = {
+      source = "bamhm182/guacamole"
+      version = "1.4.2"
+    }
+    htpasswd = {
+      source  = "loafoe/htpasswd"
+      version = "1.2.1"
+    }
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+  }
+}
+
+provider "guacamole" {}
+provider "libvirt" {}
