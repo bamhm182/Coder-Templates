836ec66edc1b17338d9db2252ce8f3171ac1cef7
Dylan Wilson <dylan@bytepen.com>
2024-11-14 19:28:41 -0500

    k3s: only fixed one source

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -12,7 +12,6 @@ resource "libvirt_network" "internal" {
 }
 
 module "node0" {
-  #source = "./modules/node"
   source = "git:::https://github.com/bamhm182/Coder-Templates.git//libvirt-k3s/modules/node?ref=wip-k3s"
   count  = data.coder_workspace.me.start_count
 
@@ -27,7 +26,7 @@ module "node0" {
 }
 
 module "node1" {
-  source = "./modules/node"
+  source = "git:::https://github.com/bamhm182/Coder-Templates.git//libvirt-k3s/modules/node?ref=wip-k3s"
   count  = data.coder_workspace.me.start_count
 
   coder_url  = data.coder_workspace.me.access_url
