a5c4967a7be12e63be7439f1ed0789cf239114ec
Dylan Wilson <dylan@bytepen.com>
2024-11-14 21:18:41 -0500

    k3s: testing multiple agents

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -10,18 +10,77 @@ resource "libvirt_network" "internal" {
   addresses = [ "127.${random_integer.network[0].result}.${random_integer.network[1].result}.0/24" ]
 }
 
-module "node" {
+module "node0" {
   source   = "./modules/node"
-  count    = length(local.agents)
 
-  agent_id      = local.agents[count.index].id
-  agent_token   = local.agents[count.index].token
+  agent_id      = coder_agent.node0.id
+  agent_token   = coder_agent.node0.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
   network_id    = libvirt_network.internal.id
   owner         = data.coder_workspace.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
-  type          = count.index == 0 ? "server" : "agent"
+  type          = "server"
   ws_name       = data.coder_workspace.me.name
-  ws_number     = count.index
+  ws_number     = 0
+}
+
+module "node1" {
+  source   = "./modules/node"
+
+  agent_id      = coder_agent.node1.id
+  agent_token   = coder_agent.node1.token
+  coder_url     = data.coder_workspace.me.access_url
+  cpu           = data.coder_parameter.cpu_count.value
+  network_id    = libvirt_network.internal.id
+  owner         = data.coder_workspace.me.name
+  ram           = (data.coder_parameter.ram_amount.value * 1024)
+  type          = "agent"
+  ws_name       = data.coder_workspace.me.name
+  ws_number     = 1
+}
+
+module "node2" {
+  source   = "./modules/node"
+
+  agent_id      = coder_agent.node2.id
+  agent_token   = coder_agent.node2.token
+  coder_url     = data.coder_workspace.me.access_url
+  cpu           = data.coder_parameter.cpu_count.value
+  network_id    = libvirt_network.internal.id
+  owner         = data.coder_workspace.me.name
+  ram           = (data.coder_parameter.ram_amount.value * 1024)
+  type          = "agent"
+  ws_name       = data.coder_workspace.me.name
+  ws_number     = 2
+}
+
+module "node3" {
+  source   = "./modules/node"
+
+  agent_id      = coder_agent.node3.id
+  agent_token   = coder_agent.node3.token
+  coder_url     = data.coder_workspace.me.access_url
+  cpu           = data.coder_parameter.cpu_count.value
+  network_id    = libvirt_network.internal.id
+  owner         = data.coder_workspace.me.name
+  ram           = (data.coder_parameter.ram_amount.value * 1024)
+  type          = "agent"
+  ws_name       = data.coder_workspace.me.name
+  ws_number     = 3
+}
+
+module "node4" {
+  source   = "./modules/node"
+
+  agent_id      = coder_agent.node4.id
+  agent_token   = coder_agent.node4.token
+  coder_url     = data.coder_workspace.me.access_url
+  cpu           = data.coder_parameter.cpu_count.value
+  network_id    = libvirt_network.internal.id
+  owner         = data.coder_workspace.me.name
+  ram           = (data.coder_parameter.ram_amount.value * 1024)
+  type          = "agent"
+  ws_name       = data.coder_workspace.me.name
+  ws_number     = 4
 }
--- libvirt-k3s/modules/node/credentials.tf
+++ libvirt-k3s/modules/node/credentials.tf
@@ -1,22 +1,27 @@
 resource "random_integer" "password_length" {
+  agent_id = var.agent_id
   min = 40
   max = 64
 }
 
 resource "random_password" "user" {
+  agent_id = var.agent_id
   length = random_integer.password_length.result
 }
 
 resource "random_password" "user_salt" {
+  agent_id = var.agent_id
   length = 8
   override_special = "./"
 }
 
 resource "htpasswd_password" "user" {
+  agent_id = var.agent_id
   password = random_password.user.result
   salt = random_password.user_salt.result
 }
 
 resource "tls_private_key" "ssh_key" {
+  agent_id = var.agent_id
   algorithm = "ED25519"
 }
