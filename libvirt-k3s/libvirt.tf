ae9d1a3fa14d4f803a4f4a8e1eb90a454a60e4e8
Dylan Wilson <dylan@bytepen.com>
2024-11-14 20:26:46 -0500

    k3s: removed all counts

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -6,18 +6,16 @@ resource "random_integer" "network" {
 
 resource "libvirt_network" "internal" {
   name      = lower("coder-${data.coder_workspace_owner.me.name}-${data.coder_workspace.me.name}-internal")
-  count     = data.coder_workspace.me.start_count
   mode      = "none"
   addresses = [ "127.${random_integer.network[0].result}.${random_integer.network[1].result}.0/24" ]
 }
 
 module "node" {
-  source   = "git::https://github.com/bamhm182/Coder-Templates.git//libvirt-k3s/modules/node?ref=wip-k3s"
-  count    = data.coder_workspace.me.start_count
+  source   = "./modules/node"
 
   coder_url  = data.coder_workspace.me.access_url
   cpu        = data.coder_parameter.cpu_count.value
-  network_id = libvirt_network.internal[0].id
+  network_id = libvirt_network.internal.id
   owner      = data.coder_workspace.me.name
   ram        = (data.coder_parameter.ram_amount.value * 1024)
   type       = "server"
