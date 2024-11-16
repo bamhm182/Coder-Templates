9277e9dfe7cfc71147f41ac3a6ce9b06d6f56adf
Dylan Wilson <dylan@bytepen.com>
2024-11-15 19:29:49 -0500

    k3s: better local network

--- libvirt-k3s/libvirt.tf
+++ libvirt-k3s/libvirt.tf
@@ -1,23 +1,18 @@
-resource "random_integer" "network" {
-  count = 2
-  min = 0
-  max = 254
-}
-
-resource "libvirt_network" "internal" {
-  name      = lower("coder-${data.coder_workspace_owner.me.name}-${data.coder_workspace.me.name}-internal")
-  mode      = "none"
-  addresses = [ "127.${random_integer.network[0].result}.${random_integer.network[1].result}.0/24" ]
+resource "libvirt_network" "private" {
+  count = data.coder_workspace.me.start_count
+  name  = lower("coder-${data.coder_workspace_owner.me.name}-${data.coder_workspace.me.name}-private")
+  mode  = "none"
 }
 
 module "node0" {
+  count = data.coder_workspace.me.start_count
   source   = "./modules/node"
 
   agent_id      = coder_agent.node0.id
   agent_token   = coder_agent.node0.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
+  network_id    = libvirt_network.private[0].id
   owner         = data.coder_workspace_owner.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
   type          = "server"
@@ -26,13 +21,14 @@ module "node0" {
 }
 
 module "node1" {
+  count = data.coder_workspace.me.start_count
   source   = "./modules/node"
 
   agent_id      = coder_agent.node1.id
   agent_token   = coder_agent.node1.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
+  network_id    = libvirt_network.private[0].id
   owner         = data.coder_workspace_owner.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
   type          = "agent"
@@ -41,13 +37,14 @@ module "node1" {
 }
 
 module "node2" {
+  count = data.coder_workspace.me.start_count
   source   = "./modules/node"
 
   agent_id      = coder_agent.node2.id
   agent_token   = coder_agent.node2.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
+  network_id    = libvirt_network.private[0].id
   owner         = data.coder_workspace_owner.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
   type          = "agent"
@@ -56,13 +53,14 @@ module "node2" {
 }
 
 module "node3" {
+  count = data.coder_workspace.me.start_count
   source   = "./modules/node"
 
   agent_id      = coder_agent.node3.id
   agent_token   = coder_agent.node3.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
+  network_id    = libvirt_network.private[0].id
   owner         = data.coder_workspace_owner.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
   type          = "agent"
@@ -71,13 +69,14 @@ module "node3" {
 }
 
 module "node4" {
+  count = data.coder_workspace.me.start_count
   source   = "./modules/node"
 
   agent_id      = coder_agent.node4.id
   agent_token   = coder_agent.node4.token
   coder_url     = data.coder_workspace.me.access_url
   cpu           = data.coder_parameter.cpu_count.value
-  network_id    = libvirt_network.internal.id
+  network_id    = libvirt_network.private[0].id
   owner         = data.coder_workspace_owner.me.name
   ram           = (data.coder_parameter.ram_amount.value * 1024)
   type          = "agent"
